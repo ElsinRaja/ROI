d2c718fd40e29ac92330094ba4d4c4e15195d4f3
elsin <elsin@NL-RLPT-16>
2024-12-02 23:32:16 +0530

    or_creation

--- src/test/java/create_organization/create.java
+++ src/test/java/create_organization/create.java
@@ -4,6 +4,7 @@ import org.testng.annotations.*;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 
 public class create {
   public Page page;
@@ -15,31 +16,47 @@ public class create {
      new LaunchOptions().setHeadless(false).setSlowMo(50));
      page = browser.newPage();
      page.navigate("http://13.59.17.106/");
+ 	page.locator("(//input[contains(@class,'MuiInputBase-input MuiOutlinedInput-input')])[1]").type("superadmin");
+ 	page.locator("(//input[contains(@class,'MuiInputBase-input MuiOutlinedInput-input')])[2]").type("1234");
+ 	page.locator("//button[@type='button']").click();
+ 	
         
  }
 @AfterMethod
 
-public void browserclose() {
-	page.close();
-}
-
-@Test
-public void login() {
-	
-	page.locator("(//input[contains(@class,'MuiInputBase-input MuiOutlinedInput-input')])[1]").type("Raghu");
-	page.locator("(//input[contains(@class,'MuiInputBase-input MuiOutlinedInput-input')])[2]").type("Punithaa#97");
-	page.locator("//button[@type='button']").click();
-	
-	
-}
+//public void browserclose() {
+//	page.close();
+//}
 
 
 @Test
-public void create_user() {
+public void add_organization() {
 	
-	page.locator("(//input[contains(@class,'MuiInputBase-input MuiOutlinedInput-input')])[1]").type("Raghu");
-	page.locator("(//input[contains(@class,'MuiInputBase-input MuiOutlinedInput-input')])[2]").type("Punithaa#97");
-	page.locator("//button[@type='button']").click();
+//	PlaywrightAssertions.assertThat(page).hasTitle("Nesh Base");
+//	page.locator("//span[normalize-space(text())='Settings']").click();
+//	page.locator("//input[contains(@class,'MuiInputBase-input MuiInput-input')]").fill("System Engineer");
+////	page.locator("  ").click(); need id
+	
+	page.locator("(//div[@class='MuiListItemText-root css-r1v2vr']//span)[1]").click();
+	page.locator("//div[@class='MuiBox-root css-12vle5y']//button[1]").click();  
+	page.locator("(//input[contains(@class,'MuiInputBase-input MuiInput-input')])[1]").type("Apollo"); //organiztion name 
+	page.locator("(//input[contains(@class,'MuiInputBase-input MuiInput-input')])[2]").type("James");  //First Name
+	page.locator("(//input[contains(@class,'MuiInputBase-input MuiInput-input')])[3]").type("Sudhan"); // Last Name
+//	 Locator option = page.locator("text='Option Text'"); // Replace with your option's text or selector
+//     option.click();
+     page.locator("(//span[normalize-space(text())='​']/following::input)[2]").type("Medical"); // Business
+     page.locator("(//span[normalize-space(text())='​']/following::input)[3]").type("No notes"); //Additional Information
+     page.locator("(//label[normalize-space(text())='Street']/following::input)[1]").type("Noble street"); //Street
+     page.locator("(//label[normalize-space(text())='City']/following::input)[1]").type("Chennai");    // City
+     page.locator("(//label[normalize-space(text())='Zipcode']/following::input)[1]").type("600025");  //Zipcode
+     page.locator("(//label[normalize-space(text())='State']/following::input)[1]").type("Tamilnadu"); //State 	
+     page.locator("(//label[normalize-space(text())='Country']/following::input)[1]").type("India");   //Country
+     page.locator("//label[normalize-space(text())='Email']/following::input").type("elsinraja4@gmail.com");
+     page.locator("//button[normalize-space(text())='Submit']").click(); //Submit
+     
+	
+	
+	
 	
 	
 }
